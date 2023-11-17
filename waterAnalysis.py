6d9022fdf7179b0bd72ac35465d7f73fd3c04f45
tyymr23 <tmrobertson2323@gmail.com>
2023-11-16 22:07:06 -0500

    results now found in results.txt

--- waterAnalysis.py
+++ waterAnalysis.py
@@ -5,69 +5,74 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-# names for columns
-columnNames = ['time', 'interval', 'temps']
-# read in csv data to analyze
-data = pd.read_csv('./wiseWaterExData.csv', names=columnNames, header=0, usecols=[0, 1, 2])
+with open('results.txt', 'w') as file:
 
-degree = chr(176)  # char for degree symbol
-tempLimit = float(input("What is the lowest acceptable temp. in " + degree + "C?  "))
-dataLength = len(data)  # indexing starts at 0
-flagOne = False
-eventCounter = 1
+    # names for columns
+    columnNames = ['time', 'interval', 'temps']
+    # read in csv data to analyze
+    data = pd.read_csv('./wiseWaterExData.csv', names=columnNames, header=0, usecols=[0, 1, 2])
 
-# checks for initial event
-for i in range(dataLength):
-    if data.temps[i] < tempLimit:
-        flagOne = True
-        break
+    degree = chr(176)  # char for degree symbol
+    tempLimit = float(input("What is the lowest acceptable temp. in " + degree + "C?  "))
+    dataLength = len(data)  # indexing starts at 0
+    flagOne = False
+    eventCounter = 1
 
-if flagOne is True:
-    # print statements for output
-    print('\n\t\t\t\t\t\t\t\tWater Temperature Information\t\t\t\t\t\n')
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------\n')
-    print('# of Event\t\tStart Time\t\t\tEnd Time\t\t\tLow Temp.\tSpike Time\t\t\tTotal Duration\n')
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------\n')
-    # initializes new variables
-    flagTwo = False
-    startTime = 0
-    endTime = 0
-    # loop through data
+    # checks for initial event
     for i in range(dataLength):
-        if data.temps[i] < tempLimit and flagTwo is False: # this indicates the start of an event
-            lowPoint = 100
-            startI = i
-            startTime = datetime.strptime(data.time[i], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
-            flagTwo = True
-        elif data.temps[i] >= tempLimit and flagTwo is True: # this indicates the end of an event
-            endTime = datetime.strptime(data.time[i], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
-            endI = i
-            for j in range(startI, endI): # finds low point in total event for spike
-                if data.temps[j] <= lowPoint:
-                    lowPoint = data.temps[j]
-                    spikeTime = datetime.strptime(data.time[j], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
-            totalTime = endTime - startTime
-            # add lambda expression to remove small intervals, such as min of 3 minutes
-            print('\t', eventCounter, '\t\t', startTime, '\t\t', endTime, '\t\t', lowPoint, '\t\t', spikeTime, '\t\t', totalTime)
-            eventCounter += 1
-            flagTwo = False
-        elif i + 1 >= dataLength and data.temps[i] < tempLimit: # this checks for edge case where last data point is included in event
-            endTime = datetime.strptime(data.time[i], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
-            endI = i
-            for j in range(startI, endI):
-                if data.temps[j] <= lowPoint:
-                    lowPoint = data.temps[j]
-                    spikeTime = datetime.strptime(data.time[j], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
-            totalTime = endTime - startTime
-            # add lambda expression to remove small intervals, such as min of 3 minutes
-            print('\t', eventCounter, '\t\t', startTime, '\t\t', endTime, '\t\t', lowPoint, '\t\t', spikeTime, '\t\t', totalTime)
-            eventCounter += 1
-            flagTwo = False
-    # print statements for output
-    print('\nThe temperature of the water DID dip below the lowest acceptable input.')
-    print('\nThe lowest limit was exceeded ' + str(eventCounter-1) + ' times.')
-else:
-    print('\nThe temperature of the water DID NOT dip below the lowest acceptable value.')
+        if data.temps[i] < tempLimit:
+            flagOne = True
+            break
+
+    if flagOne is True:
+        # print statements for output
+        print('\n\t\t\t\t\t\t\t\t\t\t\tWater Temperature Information\n', file=file)
+        print('------------------------------------------------------------------------------------------------------------------------------------------------------\n', file=file)
+        print('\t# of Event\t\tStart Time\t\t\t\tEnd Time\t\t\t\tLow Temp.\t\tSpike Time\t\t\t\tTotal Duration\n', file=file)
+        print('------------------------------------------------------------------------------------------------------------------------------------------------------\n', file=file)
+        # initializes new variables
+        flagTwo = False
+        startTime = 0
+        endTime = 0
+        # loop through data
+        for i in range(dataLength):
+            if data.temps[i] < tempLimit and flagTwo is False: # this indicates the start of an event
+                lowPoint = 100
+                startI = i
+                startTime = datetime.strptime(data.time[i], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
+                flagTwo = True
+            elif data.temps[i] >= tempLimit and flagTwo is True: # this indicates the end of an event
+                endTime = datetime.strptime(data.time[i], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
+                endI = i
+                for j in range(startI, endI): # finds low point in total event for spike
+                    if data.temps[j] <= lowPoint:
+                        lowPoint = data.temps[j]
+                        spikeTime = datetime.strptime(data.time[j], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
+                totalTime = endTime - startTime
+                # add lambda expression to remove small intervals, such as min of 3 minutes
+                print('\t{}\t\t\t\t{}\t\t{}\t\t{}\t\t\t{}\t\t{}'.format(eventCounter, startTime, endTime, lowPoint, spikeTime, totalTime), file=file)
+                eventCounter += 1
+                flagTwo = False
+            elif i + 1 >= dataLength and data.temps[i] < tempLimit: # this checks for edge case where last data point is included in event
+                endTime = datetime.strptime(data.time[i], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
+                endI = i
+                for j in range(startI, endI):
+                    if data.temps[j] <= lowPoint:
+                        lowPoint = data.temps[j]
+                        spikeTime = datetime.strptime(data.time[j], "%m/%d/%Y %H:%M:%S %p") # restructures time for later
+                totalTime = endTime - startTime
+                # add lambda expression to remove small intervals, such as min of 3 minutes
+                print('\t{}\t\t\t\t{}\t\t{}\t\t{}\t\t\t{}\t\t{}'.format(eventCounter, startTime, endTime, lowPoint, spikeTime, totalTime), file=file)
+                eventCounter += 1
+                flagTwo = False
+        # print statements for output
+        print('\nThe temperature of the water DID dip below the lowest acceptable input.', file=file)
+        print('\nThe lowest limit was exceeded {} times.'.format(str(eventCounter - 1)), file=file)
+    else:
+        print('\nThe temperature of the water DID NOT dip below the lowest acceptable value.', file=file)
+
+print('Results can be found in results.txt')
+print('Data plot being created... may take a minute or two')
 
 # plot creation for data
 plt.scatter(data.time, data.temps)
